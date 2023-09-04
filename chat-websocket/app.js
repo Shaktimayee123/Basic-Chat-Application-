22cec36115b4d5605cd9099f349d66e783d964ce
Shaktimayee Dash <142074778+Shaktimayee123@users.noreply.github.com>
2023-09-04 13:43:30 +0530

    Add files via upload
    I developed  a basic chat application using WebSocket as a project, during the internship of code clause

---
+++ chat-websocket/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require ('path')
+const { disconnect } = require('process')
+const app= express()
+const PORT = 4000 || process.env.PORT 
+const server= app.listen(PORT,()=> console.log('💭 server on port ${PORT}'))
+
+const io = require('socket.io')(server)
+
+let socketsConnected = new Set()
+app.use(express.static(path.join(__dirname,'public')))
+
+io.on('connection', onConnected)
+function onConnected(socket){
+    console.log(socket.id) 
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total',socketsConnected.size)
+
+    socket.on('disconnect',() => {
+        console.log('Socket disconnected',socket.id)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total',socketsConnected.size)
+
+    })
+
+    socket.on('message',(data) => {
+    console.log(data)
+    socket.broadcast.emit('chat-message', data)
+    })
+    socket.on('feedback',(data) => {
+        socket.broadcast.emit('feedback', data)
+    })
+}
+
+
+
